2fe5ba81105ee4ac1bf597620734b184caef9875
Stephanie Claudino Daffara <stephaniecd@berkeley.edu>
2019-04-02 17:30:57 -0700

    ARkit is working

---
+++ Assets/Scripts/ARApi.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARApi : MonoBehaviour
+{
+
+    public GameObject IOSPrefab;
+    // public GameObject AndroidPrefab;
+    private GameObject ARSession;
+
+    void Start() {
+
+        #if UNITY_EDITOR
+        Debug.Log("Unity Editor");
+        #endif
+        
+        #if UNITY_IOS
+        Debug.Log("Iphone");
+        ARSession = Instantiate(IOSPrefab);
+        #endif  
+
+        #if UNITY_ANDROID
+        Debug.Log("Android");
+        // ARSession = Instantiate(AndroidPrefab);
+        #endif  	
+
+    }
+
+}
